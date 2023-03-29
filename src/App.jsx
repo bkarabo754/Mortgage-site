dd2b6d0745d99466a49c0f47ea0509ab9a17c559
Bongani Masango <bkmasango26@gmail.com>
2023-03-29 16:03:53 +0200

    mortgage-calc

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      Hello Everyone
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { theme } from './theme'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <App />
+    <CssBaseline />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
